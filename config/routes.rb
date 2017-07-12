d4ea633953b2060d49cbabbd96facf6c105fc670
Worlanyo <worlalidamalie@gmail.com>
2017-07-12 19:04:56 +0000

    Add services page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
 
   #get 'admin/index'
 
+
   devise_for :admins , :controllers => { registrations: 'registrations' }
   resources :cows
   resources :vet_officers
   resources :farmers
+  #get 'static_pages#services'
+  get '/services', to: 'static_pages#services'
 
   devise_scope :admin do
      get '/dashboard', to: 'cows#index'
