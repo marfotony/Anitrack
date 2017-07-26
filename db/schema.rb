8f535e0e1af62434ad245e830fb8c3d1d542b829
charles agyemang <micnkru@gmail.com>
2017-07-26 02:55:15 +0000

    feat: new stuff to csend sms

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170712102442) do
 
-<<<<<<< HEAD
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-=======
->>>>>>> 28e87406b2ae827afdf2e5f22894d5a308e18716
   create_table "admins", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password",     default: "", null: false
@@ -31,13 +28,12 @@ ActiveRecord::Schema.define(version: 20170712102442) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-<<<<<<< HEAD
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-=======
+
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
->>>>>>> 28e87406b2ae827afdf2e5f22894d5a308e18716
+
   end
 
   create_table "cows", force: :cascade do |t|
@@ -48,7 +44,7 @@ ActiveRecord::Schema.define(version: 20170712102442) do
     t.string   "tag_number"
     t.string   "temperature"
     t.string   "status"
-<<<<<<< HEAD
+
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "farmer_id"
@@ -56,7 +52,7 @@ ActiveRecord::Schema.define(version: 20170712102442) do
     t.string   "vetofficer"
     t.index ["farmer_id"], name: "index_cows_on_farmer_id", using: :btree
     t.index ["vet_officers_id"], name: "index_cows_on_vet_officers_id", using: :btree
-=======
+
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "farmer_id"
@@ -64,7 +60,7 @@ ActiveRecord::Schema.define(version: 20170712102442) do
     t.string   "vetofficer"
     t.index ["VetOfficer_id"], name: "index_cows_on_VetOfficer_id"
     t.index ["farmer_id"], name: "index_cows_on_farmer_id"
->>>>>>> 28e87406b2ae827afdf2e5f22894d5a308e18716
+
   end
 
   create_table "farmers", force: :cascade do |t|
@@ -89,9 +85,9 @@ ActiveRecord::Schema.define(version: 20170712102442) do
     t.datetime "updated_at",   null: false
   end
 
-<<<<<<< HEAD
+
   add_foreign_key "cows", "farmers"
   add_foreign_key "cows", "vet_officers", column: "vet_officers_id"
-=======
->>>>>>> 28e87406b2ae827afdf2e5f22894d5a308e18716
+
+
 end
