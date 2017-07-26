1439efc93d368501e5d11decdce3111ca33e4843
charles agyemang <micnkru@gmail.com>
2017-07-26 03:20:56 +0000

    feat: cracked the text message bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,22 +15,21 @@
 //= require turbolinks
 //= require_tree .
 
-function sendEmail() {
+function sendEmail(number, content) {
   return {
   "async": true,
   "crossDomain": true,
   "url": "https://api.twilio.com/2010-04-01/Accounts/ACa19d0e8adca65c6c86e46f52d802005f/Messages.json",
   "method": "POST",
   "headers": {
-    'Access-Control-Allow-Origin': '*',
     "authorization": "Basic QUNhMTlkMGU4YWRjYTY1YzZjODZlNDZmNTJkODAyMDA1Zjo5NjViYmFhNDFmNGE4NTZjNTNjMDIxMjBkZTgxNmQ5OQ==",
-    "content-type": "application/x-www-form-urlencoded",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
 
   },
   "data": {
-    "To":   "+233541348180",
-    "From": "+19099395724",
-    "Body": "This is a test transmission"
+    "To"  :   number,
+    "From":   "+19099395724",
+    "Body":   content
   }
 }
 
@@ -53,11 +52,15 @@ var ready =  function(){
       var email           = "micnkru@gmail.com";
 
       var content =  "Hello " + name + " you have " + numberOfAnimals + " of cows so far in good health we will notify you of any wrong thing thank you"
+      var $this =  $(this);
+      $this.text("sending..");
 
-
-      $.ajax(sendEmail()).done(function (response) {
+      $.ajax(sendEmail("+233" + phoneNumber, content)).done(function (response) {
         console.log(response);
+        $this.text("sent");
       });
+
+
 
     });
 }
